275e720b218d2c428d3db666e7c5bdf2278c0767
Alhasan-Abdellatif <alhasanabdellatif@gmail.com>
2024-05-11 14:48:45 +0100

    comment libs

--- realesrgan/train.py
+++ realesrgan/train.py
@@ -2,9 +2,13 @@
 import os.path as osp
 from basicsr.train import train_pipeline
 
-import realesrgan.archs
-import realesrgan.data
-import realesrgan.models
+#import realesrgan.data
+#import realesrgan.models
+#import realesrgan.archs
+
+#import data
+#import models
+#import archs
 
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
